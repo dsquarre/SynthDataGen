bc5f6d34d9e32fb9ee714799696429c4f742fc31
dsquarre <dsquar0e@gmail.com>
2026-01-22 20:08:38 +0530

    adding regression metrics to code

--- synthpop.py
+++ synthpop.py
@@ -5,6 +5,8 @@ df = pd.read_excel('dataset.xlsx')
 df.drop(columns=['Specimen'],inplace=True)
 df.reset_index(drop=True,inplace=True)
 df.columns = df.columns.str.strip()
+#print(df.columns)
+
 #df['Specimen']
 #print(df.isnull().sum())
 
@@ -26,21 +28,30 @@ processed_data = processor.preprocess(real_df)
 cart = CARTMethod(metadata,minibucket=5, random_state=59)
 cart.fit(processed_data)
 synthetic_processed = cart.sample(100)
-synthetic_df = processor.postprocess(synthetic_processed)
-print(synthetic_df)
+cart_df = processor.postprocess(synthetic_processed)
+cart_df.to_excel('cart_df.xlsx') 
 
 gc = GaussianCopulaMethod(metadata)
 gc.fit(processed_data)
 synth_gc = gc.sample(100)
 gc_df = processor.postprocess(synth_gc)
-print(gc_df)
-
+gc_df.to_excel('gc_df.xlsx') 
 
 from py_synthpop.synthpop.metrics.diagnostic_report import MetricsReport
-report = MetricsReport(real_df, synthetic_df, metadata)
-report_df = report.generate_report()
-print(report_df)
+report_cart = MetricsReport(real_df, cart_df, metadata)
+report_df_cart = report_cart.generate_report()
+#print(report_df)
+report_df_cart.to_excel('cart_report.xlsx')
 
+report_gc = MetricsReport(real_df, gc_df, metadata)
+report_df_gc = report_gc.generate_report()
+report_df_gc.to_excel('gc_report.xlsx')
 from py_synthpop.synthpop.metrics.efficacy_metrics import EfficacyMetrics
-reg_efficacy = EfficacyMetrics(task='regression', target_column="income")
-reg_metrics = reg_efficacy.evaluate(real_df, synthetic_df)
+reg_efficacy = EfficacyMetrics(task='regression', target_column="Pu [kN]")
+print('Regression Efficacy Metrics for CART:')
+reg_metrics_cart = reg_efficacy.evaluate(real_df, cart_df)
+print(reg_metrics_cart,'\n')
+
+print('Regression Efficacy Metrics for GC:')
+reg_metrics_gc = reg_efficacy.evaluate(real_df, gc_df)
+print(reg_metrics_gc)
